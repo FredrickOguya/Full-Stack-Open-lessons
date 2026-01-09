0189fa558f0633a1841a46978ee9bcc14f9aaa61
Fredrick Onyango <onyangofredrickoguya@gmail.com>
2026-01-09 14:54:25 +0300

    Using database in route handlers

--- backend/index.js
+++ backend/index.js
@@ -1,29 +1,39 @@
+require('dotenv').config()
 const express = require('express')
 const app = express();
 const cors = require('cors')
+const mongoose = require('mongoose');
+const password = process.argv[2]
+const url = `mongodb+srv://onyangofredrickoguya:${password}@cluster0.hxovx14.mongodb.net/noteApp?appName=Cluster0`
+
 
 app.use(express.json())
 app.use(cors())
-
 app.use(express.static('dist'))
 
-let notes = [
-  {
-    id: "1",
-    content: "HTML is easy",
-    important: true
-  },
-  {
-    id: "2",
-    content: "Browser can execute only javascript",
-    important: false
-  },
-  {
-    id: "3",
-    content: "GET and POST are the most important methods of HTTP protocol",
-    important: true
+mongoose.set('strictQuery',false)
+mongoose.connect(url, { family: 4})
+
+const noteSchema = new mongoose.Schema({
+  content: String,
+  important: Boolean,
+})
+
+noteSchema.set('toJSON', {
+  transform: (document, returnedObject)=> {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
   }
-];
+})
+
+const Note = require('./models/note')
+
+
+
+let notes = [];
+
+
 
 const generateId = () => {
   
@@ -35,21 +45,17 @@ const generateId = () => {
 
 
 app.get('/api/notes', (request, response) => {
-
-
-  response.json(notes);
+  Note.find({}).then(notes => {
+    response.json(notes)
+  })
 })
 
 
 
 app.get('/api/notes/:id',(request,response)=> {
-  const id = request.params.id;
-  const note = notes.find(note=> note.id === id)
-  if(note){
-    response.json(note);
-  }else {
-    response.status(404).end()
-  }
+  Note.findById(request.params.id).then(note => {
+    response.json(note)
+  })
 })
 
 app.delete('/api/notes/:id',(request,response) => {
@@ -66,17 +72,17 @@ app.post('/api/notes',(request,response)=> {
     })
   }
 
-  const note = {
+  const note = new Note({
     content: body.content,
     important: body.important || false,
-    id: generateId(),
-  }
+  })
 
-  notes = notes.concat(note);
-  response.json(note);
+  note.save().then(savedNote => {
+    response.json(savedNote)
+  })
 })
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT 
 app.listen(PORT, ()=> {
   console.log(`Server running on port ${PORT}`)
 })
@@ -96,5 +102,3 @@ app.put('/api/notes/:id', (request,response)=> {
 
 
 })
-
-//mongodb+srv://onyangofredrickoguya:<db_password>@cluster0.hxovx14.mongodb.net/?appName=Cluster0
