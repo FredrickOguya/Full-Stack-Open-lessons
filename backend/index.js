fc02f0575d219e4c80d1c20078f39c89ce372b5e
Fredrick Onyango <onyangofredrickoguya@gmail.com>
2026-01-25 12:06:54 +0300

    expanding the error handler

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,11 @@ mongoose.set('strictQuery',false)
 mongoose.connect(url, { family: 4})
 
 const noteSchema = new mongoose.Schema({
-  content: String,
+  content: {
+    type: String,
+    minLength: 5,
+    required: true
+  } ,
   important: Boolean,
 })
 
@@ -30,6 +34,8 @@ const errorHandler = (error, request, response, next) => {
 
   if (error.name === 'CastError'){
     return response.status(400).send({ error: 'malformatted id'})
+  } else if(error.name === 'ValidationError'){
+    return response.status(400).json({error: error.message})
   }
 
   next(error)
@@ -66,7 +72,7 @@ app.delete('/api/notes/:id',(request,response,next) => {
    })
    .catch(error => next(error))
 })
-app.post('/api/notes',(request,response)=> {
+app.post('/api/notes',(request,response, next)=> {
   const body = request.body
   if(!body.content){
     return response.status(400).json({
@@ -82,6 +88,7 @@ app.post('/api/notes',(request,response)=> {
   note.save().then(savedNote => {
     response.json(savedNote)
   })
+  .catch(error => next(error))
 })
 
 const PORT = process.env.PORT 
